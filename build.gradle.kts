fa51aaf60e0126aee16cff66729dbb5e6facd4bc
Levy Albuquerque <levyddsa@gmail.com>
2024-07-31 13:02:34 -0300

    feat(build): add rust_android gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@ group = "tech.turso"
 version = "0.1"
 
 plugins {
-
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.rust.android) apply false
 }
--- libsql_android/build.gradle.kts
+++ libsql_android/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.rust.android)
 }
+
+apply(plugin = "org.mozilla.rust-android-gradle.rust-android")
 
 android {
     namespace = "tech.turso.libsql"
@@ -10,16 +13,16 @@ android {
         minSdk = 21
         consumerProguardFiles("consumer-rules.pro")
 
+        // follow AGP 8.4 default: https://developer.android.com/build/releases/gradle-plugin#compatibility
+        ndkVersion = "26.1.10909125"
+
         ndk {
             abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
         }
-
-        aarMetadata {
-            minCompileSdk = 21
-        }
     }
+
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -27,22 +30,28 @@ android {
             )
         }
     }
+
+    // follow AGP 8.4 default: https://developer.android.com/build/releases/gradle-plugin#compatibility
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    externalNativeBuild {
-        ndkBuild {
-            path = file("src/main/jni/Android.mk")
-        }
-    }
-    publishing {
-        singleVariant("release") {
-            withSourcesJar()
-        }
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
-dependencies {
+cargo {
+    module = "./src/main/rust/"
+    libname = "libsql_android"
+    targets = listOf("arm", "arm64", "x86", "x86_64")
+    exec = { spec, _ ->
+        spec.environment("ANDROID_NDK_HOME", android.ndkDirectory.path)
 
+        // default clang version for NDK 26.1.10909125, NDK 27^ uses 18
+        spec.environment("NDK_CLANG_VERSION", 17)
+    }
+}
+
+// taken from https://github.com/firezone/firezone/blob/e856dc5eb2052eff48212b94a6ebf86b2c1e67df/kotlin/android/app/build.gradle.kts#L247-L250
+tasks.matching { it.name.matches(Regex("merge.*JniLibFolders")) }.configureEach {
+    inputs.dir(layout.buildDirectory.file("rustJniLibs/android"))
+    dependsOn("cargoBuild")
 }
