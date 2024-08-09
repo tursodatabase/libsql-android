24baac2991fe2cec528b72f3687e18323f64dda8
Levy A. <levyddsa@gmail.com>
2024-08-09 16:21:48 -0300

    migrate to kotlin

--- libsql/src/main/rust/src/lib.rs
+++ libsql/src/main/rust/src/lib.rs
@@ -163,7 +163,7 @@ pub fn nativeClose(_: JNIEnv, _: JClass, ptr: jlong) {
     drop(unsafe { Box::from_raw(ptr as *mut Database) });
 }
 
-#[jni_fn("tech.turso.libsql.EmbeddedReplicaDatabase")]
+#[jni_fn("tech.turso.libsql.Database")]
 pub fn nativeSync(mut env: JNIEnv, _: JClass, ptr: jlong) {
     let db = ManuallyDrop::new(unsafe { Box::from_raw(ptr as *mut Database) });
 
