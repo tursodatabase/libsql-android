8c9e8a1f7ba3d2355fff354de9b37576951a8522
Levy A. <levyddsa@gmail.com>
2024-08-09 19:47:35 -0300

    add double close test

--- libsql/src/androidTest/java/tech/turso/libsql/LibsqlTest.java
+++ libsql/src/androidTest/java/tech/turso/libsql/LibsqlTest.java
@@ -1,5 +1,8 @@
 package tech.turso.libsql;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import java.util.Map;
 import org.junit.Test;
@@ -10,15 +13,27 @@ public class LibsqlTest {
     @Test
     public void queryEmptyParameters() {
         try (var db = Libsql.open(":memory:");
-                var conn = db.connect(); ) {
+                var conn = db.connect() ) {
             conn.query("select 1");
+        }
+    }
+
+    @Test
+    public void failOpenEmptyName() {
+        try {
+            var db = Libsql.open(":memory:");
+            db.close();
+            db.close();
+            fail("Successfully closed the same db two times");
+        } catch (Throwable e) {
+            assertNotNull(e);
         }
     }
 
     @Test
     public void queryNamedParameters() {
         try (var db = Libsql.open(":memory:");
-                var conn = db.connect(); ) {
+                var conn = db.connect() ) {
             conn.query("select :a", Map.of("a", Value.newBuilder().setInteger(1).build()));
         }
     }
@@ -26,7 +41,7 @@ public class LibsqlTest {
     @Test
     public void queryPositionalParameters() {
         try (var db = Libsql.open(":memory:");
-                var conn = db.connect(); ) {
+                var conn = db.connect() ) {
             conn.query("select ?", Value.newBuilder().setInteger(1).build());
         }
     }
