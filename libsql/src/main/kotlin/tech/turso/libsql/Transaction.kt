647b6c1662d055d87bae5efd02424707701347f8
Levy A. <levyddsa@gmail.com>
2024-08-15 10:45:24 -0300

    implement connection interface for Transaction

--- libsql/src/main/kotlin/tech/turso/libsql/Transaction.kt
+++ libsql/src/main/kotlin/tech/turso/libsql/Transaction.kt
@@ -1,38 +1,94 @@
-import tech.turso.libsql.Connection
-import tech.turso.libsql.Rows
+
+package tech.turso.libsql
+
+import tech.turso.libsql.proto.NamedParameters
+import tech.turso.libsql.proto.Parameters
+import tech.turso.libsql.proto.PositionalParameters
 import tech.turso.libsql.proto.Value
 
 class Transaction internal constructor(private var inner: Long) : Connection {
     override fun execute(sql: String) {
-        TODO("Not yet implemented")
+        require(this.inner != 0L) { "Attempted to execute with a closed Transaction" }
+        nativeExecute(this.inner, sql, byteArrayOf())
     }
 
     override fun execute(sql: String, params: Map<String, Value>) {
-        TODO("Not yet implemented")
+        require(this.inner != 0L) { "Attempted to execute with a closed Transaction" }
+
+        val buf =
+            Parameters.newBuilder()
+                .setNamed(NamedParameters.newBuilder().putAllParameters(params))
+                .build()
+                .toByteArray()
+
+        nativeExecute(this.inner, sql, buf)
     }
 
-    override fun execute(sql: String, vararg params: Value) {
-        TODO("Not yet implemented")
+    override fun execute(
+        sql: String,
+        vararg params: Value,
+    ) {
+        require(this.inner != 0L) { "Attempted to execute with a closed Transaction" }
+
+        val buf =
+            Parameters.newBuilder()
+                .setPositional(
+                    PositionalParameters.newBuilder()
+                        .addAllParameters(params.asList()),
+                )
+                .build()
+                .toByteArray()
+
+        nativeQuery(this.inner, sql, buf)
     }
 
     override fun query(sql: String): Rows {
-        TODO("Not yet implemented")
+        require(this.inner != 0L) { "Attempted to query with a closed Transaction" }
+        return Rows(nativeQuery(this.inner, sql, byteArrayOf()))
     }
 
-    override fun query(sql: String, params: Map<String, Value>): Rows {
-        TODO("Not yet implemented")
+    override fun query(
+        sql: String,
+        params: Map<String, Value>,
+    ): Rows {
+        require(this.inner != 0L) { "Attempted to query with a closed Transaction" }
+
+        val buf =
+            Parameters.newBuilder()
+                .setNamed(NamedParameters.newBuilder().putAllParameters(params))
+                .build()
+                .toByteArray()
+
+        return Rows(nativeQuery(this.inner, sql, buf))
     }
 
-    override fun query(sql: String, vararg params: Value): Rows {
-        TODO("Not yet implemented")
+    override fun query(
+        sql: String,
+        vararg params: Value,
+    ): Rows {
+        require(this.inner != 0L) { "Attempted to query with a closed Transaction" }
+
+        val buf =
+            Parameters.newBuilder()
+                .setPositional(
+                    PositionalParameters.newBuilder()
+                        .addAllParameters(params.asList()),
+                )
+                .build()
+                .toByteArray()
+
+        return Rows(nativeQuery(this.inner, sql, buf))
     }
 
     override fun transaction(): Transaction {
-        TODO("Not yet implemented")
+        require(this.inner != 0L) { "Transaction already closed" }
+        return Transaction(nativeTransaction(this.inner))
     }
 
     override fun close() {
-        TODO("Not yet implemented")
+        require(this.inner != 0L) { "Transaction already closed" }
+        nativeClose(this.inner)
+        this.inner = 0L
     }
 
     private external fun nativeExecute(
