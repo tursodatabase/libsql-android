9197150e98709b6dc7cc46b642dbd18ce1fd5984
Levy A. <levyddsa@gmail.com>
2024-08-15 08:46:10 -0300

    better error handling

--- libsql/src/main/rust/src/lib.rs
+++ libsql/src/main/rust/src/lib.rs
@@ -62,58 +62,43 @@ lazy_static! {
 
 #[jni_fn("tech.turso.libsql.Libsql")]
 pub fn nativeOpenLocal(mut env: JNIEnv, _: JClass, path: JString) -> jlong {
-    let path = match env.get_string(&path) {
-        Ok(path) => path,
+    match (|| -> anyhow::Result<Database> {
+        let path = env.get_string(&path)?;
+        Ok(RT.block_on(Builder::new_local(&*path.to_string_lossy()).build())?)
+    })() {
+        Ok(db) => Box::into_raw(Box::new(db)) as jlong,
         Err(err) => {
             env.throw(err.to_string()).unwrap();
-            return ptr::null_mut::<Database>() as jlong;
+            ptr::null_mut::<Database>() as jlong
         }
-    };
-
-    let db = match RT.block_on(Builder::new_local(&*path.to_string_lossy()).build()) {
-        Ok(db) => db,
-        Err(err) => {
-            env.throw(err.to_string()).unwrap();
-            return ptr::null_mut::<Database>() as jlong;
-        }
-    };
-
-    Box::into_raw(Box::new(db)) as jlong
+    }
 }
 
 #[jni_fn("tech.turso.libsql.Libsql")]
 pub fn nativeOpenRemote(mut env: JNIEnv, _: JClass, url: JString, auth_token: JString) -> jlong {
-    let url = match env.get_string(&url) {
-        Ok(path) => path,
-        Err(err) => {
-            env.throw(err.to_string()).unwrap();
-            return ptr::null_mut::<Database>() as jlong;
-        }
-    };
+    match (|| -> anyhow::Result<Database> {
+        let url = env.get_string(&url)?;
+        let auth_token = env.get_string(&auth_token)?;
 
-    let auth_token = match env.get_string(&auth_token) {
-        Ok(path) => path,
-        Err(err) => {
-            env.throw(err.to_string()).unwrap();
-            return ptr::null_mut::<Database>() as jlong;
-        }
-    };
+        let connector = hyper_rustls::HttpsConnectorBuilder::new()
+            .with_webpki_roots()
+            .https_or_http()
+            .enable_http1()
+            .build();
 
-    let connector = hyper_rustls::HttpsConnectorBuilder::new()
-        .with_webpki_roots()
-        .https_or_http()
-        .enable_http1()
-        .build();
+        let db = RT.block_on(
+            Builder::new_remote(url.into(), auth_token.into())
+                .connector(connector)
+                .build(),
+        );
 
-    let db = RT.block_on(
-        Builder::new_remote(url.into(), auth_token.into())
-            .connector(connector)
-            .build(),
-    );
-
-    match db {
+        Ok(db?)
+    })() {
         Ok(db) => Box::into_raw(Box::new(db)) as jlong,
-        Err(_) => ptr::null_mut::<Database>() as jlong,
+        Err(err) => {
+            env.throw(err.to_string()).unwrap();
+            ptr::null_mut::<Database>() as jlong
+        }
     }
 }
 
@@ -125,43 +110,25 @@ pub fn nativeOpenEmbeddedReplica(
     url: JString,
     auth_token: JString,
 ) -> jlong {
-    let path = match env.get_string(&path) {
-        Ok(path) => path,
-        Err(err) => {
-            env.throw(err.to_string()).unwrap();
-            return ptr::null_mut::<Database>() as jlong;
-        }
-    };
+    match (|| -> anyhow::Result<Database> {
+        let path = env.get_string(&path)?;
+        let url = env.get_string(&url)?;
+        let auth_token = env.get_string(&auth_token)?;
 
-    let url = match env.get_string(&url) {
-        Ok(path) => path,
-        Err(err) => {
-            env.throw(err.to_string()).unwrap();
-            return ptr::null_mut::<Database>() as jlong;
-        }
-    };
+        let connector = hyper_rustls::HttpsConnectorBuilder::new()
+            .with_webpki_roots()
+            .https_or_http()
+            .enable_http1()
+            .build();
 
-    let auth_token = match env.get_string(&auth_token) {
-        Ok(path) => path,
-        Err(err) => {
-            env.throw(err.to_string()).unwrap();
-            return ptr::null_mut::<Database>() as jlong;
-        }
-    };
+        let db = RT.block_on(
+            Builder::new_remote_replica(&*path.to_string_lossy(), url.into(), auth_token.into())
+                .connector(connector)
+                .build(),
+        );
 
-    let connector = hyper_rustls::HttpsConnectorBuilder::new()
-        .with_webpki_roots()
-        .https_or_http()
-        .enable_http1()
-        .build();
-
-    let db = RT.block_on(
-        Builder::new_remote_replica(&*path.to_string_lossy(), url.into(), auth_token.into())
-            .connector(connector)
-            .build(),
-    );
-
-    match db {
+        Ok(db?)
+    })() {
         Ok(db) => Box::into_raw(Box::new(db)) as jlong,
         Err(err) => {
             env.throw(err.to_string()).unwrap();
@@ -190,7 +157,6 @@ pub fn nativeClose(_: JNIEnv, _: JClass, ptr: jlong) {
 #[jni_fn("tech.turso.libsql.EmbeddedReplicaDatabase")]
 pub fn nativeSync(mut env: JNIEnv, _: JClass, ptr: jlong) {
     let db = ManuallyDrop::new(unsafe { Box::from_raw(ptr as *mut Database) });
-
     match RT.block_on(db.sync()) {
         Ok(_) => (),
         Err(err) => env.throw(err.to_string()).unwrap(),
@@ -199,58 +165,38 @@ pub fn nativeSync(mut env: JNIEnv, _: JClass, ptr: jlong) {
 
 #[jni_fn("tech.turso.libsql.Connection")]
 pub fn nativeExecute(mut env: JNIEnv, _: JClass, conn: jlong, sql: JString, buf: JByteArray) {
-    let sql = match env.get_string(&sql) {
-        Ok(path) => path,
-        Err(err) => {
-            env.throw(err.to_string()).unwrap();
-            return;
-        }
-    };
+    match (|| -> anyhow::Result<u64> {
+        let sql = env.get_string(&sql)?;
 
-    let conn = conn as *mut Connection;
-    let conn = ManuallyDrop::new(unsafe { Box::from_raw(conn) });
+        let conn = conn as *mut Connection;
+        let conn = ManuallyDrop::new(unsafe { Box::from_raw(conn) });
 
-    let buf = match env.convert_byte_array(buf) {
-        Ok(buf) => buf,
-        Err(err) => {
-            env.throw(err.to_string()).unwrap();
-            return;
-        }
-    };
+        let buf = env.convert_byte_array(buf)?;
 
-    use proto::*; // I don't like this, but the alternaive is too verbose
+        let proto::Parameters { parameters } = proto::Parameters::decode(buf.as_slice())?;
 
-    let Parameters { parameters } = match Parameters::decode(buf.as_slice()) {
-        Ok(params) => params,
-        Err(err) => {
-            env.throw(err.to_string()).unwrap();
-            return;
-        }
-    };
+        use proto::{parameters::Parameters, NamedParameters, PositionalParameters};
 
-    match parameters {
-        Some(parameters::Parameters::Named(NamedParameters { parameters })) => {
-            let parameters = parameters
-                .into_iter()
-                .map(|(k, v)| (k, v.into()))
-                .collect::<Vec<(String, libsql::Value)>>();
+        Ok(match parameters {
+            Some(Parameters::Named(NamedParameters { parameters })) => {
+                let parameters = parameters
+                    .into_iter()
+                    .map(|(k, v)| (k, v.into()))
+                    .collect::<Vec<(String, libsql::Value)>>();
 
-            if let Err(err) = RT.block_on(conn.execute(&sql.to_string_lossy(), parameters)) {
-                env.throw(err.to_string()).unwrap();
+                RT.block_on(conn.execute(&sql.to_string_lossy(), parameters))?
             }
-        }
-        Some(parameters::Parameters::Positional(PositionalParameters { parameters })) => {
-            let parameters: Vec<libsql::Value> = parameters.into_iter().map(|v| v.into()).collect();
+            Some(Parameters::Positional(PositionalParameters { parameters })) => {
+                let parameters: Vec<libsql::Value> =
+                    parameters.into_iter().map(|v| v.into()).collect();
 
-            if let Err(err) = RT.block_on(conn.execute(&sql.to_string_lossy(), parameters)) {
-                env.throw(err.to_string()).unwrap();
+                RT.block_on(conn.execute(&sql.to_string_lossy(), parameters))?
             }
-        }
-        None => {
-            if let Err(err) = RT.block_on(conn.execute(&sql.to_string_lossy(), ())) {
-                env.throw(err.to_string()).unwrap();
-            }
-        }
+            None => RT.block_on(conn.execute(&sql.to_string_lossy(), ()))?,
+        })
+    })() {
+        Ok(_) => (),
+        Err(err) => env.throw(err.to_string()).unwrap(),
     }
 }
 
@@ -262,71 +208,42 @@ pub fn nativeQuery(
     sql: JString,
     buf: JByteArray,
 ) -> jlong {
-    let sql = match env.get_string(&sql) {
-        Ok(path) => path,
-        Err(err) => {
-            env.throw(err.to_string()).unwrap();
-            return ptr::null_mut::<Rows>() as jlong;
-        }
-    };
+    match (|| -> anyhow::Result<Rows> {
+        let sql = env.get_string(&sql)?;
 
-    let conn = conn as *mut Connection;
-    let conn = ManuallyDrop::new(unsafe { Box::from_raw(conn) });
+        let conn = conn as *mut Connection;
+        let conn = ManuallyDrop::new(unsafe { Box::from_raw(conn) });
 
-    let buf = match env.convert_byte_array(buf) {
-        Ok(buf) => buf,
-        Err(err) => {
-            env.throw(err.to_string()).unwrap();
-            return ptr::null_mut::<Rows>() as jlong;
-        }
-    };
+        let buf = env.convert_byte_array(buf)?;
 
-    use proto::*; // I don't like this, but the alternaive is too verbose
+        let proto::Parameters { parameters } = proto::Parameters::decode(buf.as_slice())?;
 
-    let Parameters { parameters } = match Parameters::decode(buf.as_slice()) {
-        Ok(params) => params,
-        Err(err) => {
-            env.throw(err.to_string()).unwrap();
-            return ptr::null_mut::<Rows>() as jlong;
-        }
-    };
+        use proto::{parameters::Parameters, NamedParameters, PositionalParameters};
 
-    match parameters {
-        Some(parameters::Parameters::Named(NamedParameters { parameters })) => {
-            let parameters = parameters
-                .into_iter()
-                .map(|(k, v)| (k, v.into()))
-                .collect::<Vec<(String, libsql::Value)>>();
+        Ok(match parameters {
+            Some(Parameters::Named(NamedParameters { parameters })) => {
+                let parameters = parameters
+                    .into_iter()
+                    .map(|(k, v)| (k, v.into()))
+                    .collect::<Vec<(String, libsql::Value)>>();
 
-            return match RT.block_on(conn.query(&sql.to_string_lossy(), parameters)) {
-                Ok(row) => Box::into_raw(Box::new(row)) as jlong,
-                Err(err) => {
-                    env.throw(err.to_string()).unwrap();
-                    return ptr::null_mut::<Rows>() as jlong;
-                }
-            };
-        }
-        Some(parameters::Parameters::Positional(PositionalParameters { parameters })) => {
-            let parameters: Vec<libsql::Value> = parameters.into_iter().map(|v| v.into()).collect();
-
-            return match RT.block_on(conn.query(&sql.to_string_lossy(), parameters)) {
-                Ok(row) => Box::into_raw(Box::new(row)) as jlong,
-                Err(err) => {
-                    env.throw(err.to_string()).unwrap();
-                    return ptr::null_mut::<Rows>() as jlong;
-                }
-            };
-        }
-        None => {
-            return match RT.block_on(conn.query(&sql.to_string_lossy(), ())) {
-                Ok(row) => Box::into_raw(Box::new(row)) as jlong,
-                Err(err) => {
-                    env.throw(err.to_string()).unwrap();
-                    return ptr::null_mut::<Rows>() as jlong;
-                }
+                RT.block_on(conn.query(&sql.to_string_lossy(), parameters))?
             }
+            Some(Parameters::Positional(PositionalParameters { parameters })) => {
+                let parameters: Vec<libsql::Value> =
+                    parameters.into_iter().map(|v| v.into()).collect();
+
+                RT.block_on(conn.query(&sql.to_string_lossy(), parameters))?
+            }
+            None => RT.block_on(conn.query(&sql.to_string_lossy(), ()))?,
+        })
+    })() {
+        Ok(row) => Box::into_raw(Box::new(row)) as jlong,
+        Err(err) => {
+            env.throw(err.to_string()).unwrap();
+            return ptr::null_mut::<Rows>() as jlong;
         }
-    };
+    }
 }
 
 #[jni_fn("tech.turso.libsql.Connection")]
@@ -335,39 +252,28 @@ pub fn nativeClose(_: JNIEnv, _: JClass, ptr: jlong) {
 }
 
 #[jni_fn("tech.turso.libsql.Rows")]
-pub fn nativeNext(mut env: JNIEnv, _: JClass, ptr: jlong) -> jbyteArray {
-    let mut rows = ManuallyDrop::new(unsafe { Box::from_raw(ptr as *mut Rows) });
+pub fn nativeNext(mut env: JNIEnv, _: JClass, rows: jlong) -> jbyteArray {
+    match (|| -> anyhow::Result<JByteArray> {
+        let rows = rows as *mut Rows;
+        let mut rows = ManuallyDrop::new(unsafe { Box::from_raw(rows) });
 
-    let row = match RT.block_on(rows.next()) {
-        Ok(row) => row.unwrap(),
+        let mut values = Vec::<proto::Value>::new();
+
+        if let Some(row) = RT.block_on(rows.next())? {
+            for i in 0..rows.column_count() {
+                let value = row.get_value(i)?;
+                values.push(value.into());
+            }
+        }
+
+        Ok(env.byte_array_from_slice(proto::Row { values }.encode_to_vec().as_slice())?)
+    })() {
+        Ok(byte_array) => byte_array.into_raw(),
         Err(err) => {
             env.throw(err.to_string()).unwrap();
             return JByteArray::default().into_raw();
         }
-    };
-
-    let mut values = Vec::<proto::Value>::new();
-    for i in 0..rows.column_count() {
-        let value = match row.get_value(i) {
-            Ok(value) => value,
-            Err(err) => {
-                env.throw(err.to_string()).unwrap();
-                return JByteArray::default().into_raw();
-            }
-        };
-        values.push(value.into());
     }
-
-    let byte_array =
-        match env.byte_array_from_slice(proto::Row { values }.encode_to_vec().as_slice()) {
-            Ok(row) => row,
-            Err(err) => {
-                env.throw(err.to_string()).unwrap();
-                return JByteArray::default().into_raw();
-            }
-        };
-
-    return byte_array.into_raw();
 }
 
 #[jni_fn("tech.turso.libsql.Rows")]
