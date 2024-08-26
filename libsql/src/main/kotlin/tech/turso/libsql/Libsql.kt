6d81877a41f3e24b677d6f509cfab1a9face48f7
Levy A. <levyddsa@gmail.com>
2024-08-26 18:32:37 -0300

    make Rows iterable

--- libsql/src/main/kotlin/tech/turso/libsql/Libsql.kt
+++ libsql/src/main/kotlin/tech/turso/libsql/Libsql.kt
@@ -1,5 +1,6 @@
 package tech.turso.libsql
 
+import com.google.protobuf.ByteString
 import tech.turso.libsql.proto.Value
 
 fun Any?.toValue(): Value =
@@ -9,6 +10,7 @@ fun Any?.toValue(): Value =
         is String -> Value.newBuilder().setText(this).build()
         is Float -> Value.newBuilder().setReal(this.toDouble()).build()
         is Double -> Value.newBuilder().setReal(this).build()
+        is ByteArray -> Value.newBuilder().setBlob(ByteString.copyFrom(this)).build()
         null -> Value.newBuilder().setNull(Value.Null.newBuilder().build()).build()
         else -> {
             throw IllegalArgumentException("Type not supported")
