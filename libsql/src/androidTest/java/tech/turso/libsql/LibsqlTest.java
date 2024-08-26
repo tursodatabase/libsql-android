3b18bc0c6eb16ddef85b22859151bb70c470c196
Levy A. <levyddsa@gmail.com>
2024-08-26 17:25:43 -0300

    fix List not imported

--- libsql/src/androidTest/java/tech/turso/libsql/LibsqlTest.java
+++ libsql/src/androidTest/java/tech/turso/libsql/LibsqlTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import java.util.List;
 import java.util.Map;
 import org.junit.Test;
 import org.junit.runner.RunWith;
